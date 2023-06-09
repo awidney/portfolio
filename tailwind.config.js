796ca6f80cd3fd68548ce72f461bd9b492981dc1
Adam Widney <awidney@my.bcit.ca>
2023-06-09 13:21:04 -0700

    Add custom fonts, colours, animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				caveat: ['Caveat', 'cursive'],
+				'nunito-sans': ['Nunito Sans', 'sans-serif'],
+			},
+			colors: {
+				'profile-red': '#800000',
+				'project-blue': '#002134',
+			},
+			keyframes: {
+				'fade-in': {
+					to: { opacity: '1' },
+				},
+			},
+			animation: {
+				'fade-in': 'fade-in 1s ease forwards',
+			},
+		},
+	},
+	plugins: [require('tailwindcss-animation-delay')],
 }
