4cb1d080036ef67631608c9e1e2832465b52f78e
Adam Widney <awidney@my.bcit.ca>
2023-06-18 19:57:55 -0700

    Remove useInView hook; Replace article with li tag

--- src/components/StackItem.tsx
+++ src/components/StackItem.tsx
@@ -1,5 +1,3 @@
-import { useInView } from 'react-intersection-observer'
-
 interface StackItemProps {
 	technology: string
 	description: string
@@ -11,22 +9,17 @@ const StackItem = ({
 	description,
 	IconComponent,
 }: StackItemProps) => {
-	const [ref, inView] = useInView({ threshold: 1 })
-
 	return (
-		<article
-			ref={ref}
-			className={`${inView ? 'show' : ''} hide mb-6 flex items-start gap-4`}
-		>
-			<span className='shrink-0 rounded-lg bg-gray-800 p-4 text-xl'>
+		<li className='mb-8 flex items-start gap-4 last:mb-0'>
+			<span className='shrink-0 rounded-lg bg-zinc-900 p-3 text-xl'>
 				{IconComponent && <IconComponent />}
 			</span>
 
 			<div>
-				<h3 className='mb-0 normal-case'>{technology}</h3>
+				<h3 className='mb-0'>{technology}</h3>
 				<p className='mt-1'>{description}</p>
 			</div>
-		</article>
+		</li>
 	)
 }
 
