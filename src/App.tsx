6089cbae68c8db27c07d44b8d6d289edc8a1a6d2
Adam Widney <awidney@my.bcit.ca>
2023-06-18 21:11:42 -0700

    Import Projects, Footer, Rays components; Add max width

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import Profile from './components/Profile'
-import ProjectsIntro from './components/ProjectsIntro'
+import Projects from './components/Projects'
 import Stack from './components/Stack'
+import Footer from './components/Footer'
+import Rays from './components/Rays'
 
 function App() {
 	const restBase = 'http://localhost/wp-portfolio/wp-json/wp/v2/'
 
 	return (
-		<div className='px-4'>
-			<Profile restBase={restBase} />
-			<ProjectsIntro restBase={restBase} />
-			<Stack restBase={restBase} />
+		<div className='mx-auto max-w-[33rem] animate-fade-in px-4 opacity-0 animation-delay-700'>
+			<main>
+				<div className='pointer-events-none absolute inset-0 select-none overflow-hidden'>
+					<Rays />
+				</div>
+				<div>
+					<Profile restBase={restBase} />
+					<Projects restBase={restBase} />
+					<Stack restBase={restBase} />
+				</div>
+			</main>
+			<footer>
+				<Footer restBase={restBase} />
+			</footer>
 		</div>
 	)
 }
