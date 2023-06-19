c92bff89cd9a7355a66df7beede83e9210be6313
Adam Widney <awidney@my.bcit.ca>
2023-06-18 20:10:32 -0700

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+
+interface FooterData {
+	acf: {
+		email: string
+	}
+}
+
+interface FooterProps {
+	restBase: string
+}
+
+const Footer = ({ restBase }: FooterProps) => {
+	const restPath = restBase + 'pages/6'
+	const [data, setData] = useState<FooterData | null>(null)
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(restPath)
+				const jsonData = await response.json()
+				setData(jsonData)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		fetchData()
+	}, [restPath])
+
+	return (
+		<>
+			{data && (
+				<a
+					className='my-14 inline-block text-sm hover:text-white hover:underline'
+					href={`mailto:${data.acf.email}`}
+				>
+					{data.acf.email}
+				</a>
+			)}
+		</>
+	)
+}
+
+export default Footer
